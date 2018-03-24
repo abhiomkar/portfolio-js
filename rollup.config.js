e55b790ddb843e266d950b1bfcbe362ed2513aa7
Abhinay Omkar <abhiomkar@gmail.com>
2018-03-25 00:40:54 +0530

    Cleaned up rollup config. CSS now only outputs to single file

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-import cssnano from 'cssnano';
 import url from 'postcss-url';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-
-const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
@@ -13,25 +10,30 @@ export default [
       {
         file: './dist/portfolio.js',
         format: 'cjs',
+        sourcemap: true,
       },
       {
         file: './dist/portfolio.umd.js',
         format: 'umd',
         name: 'Portfolio',
+        sourcemap: true,
       },
       {
         file: './dist/portfolio.es.js',
         format: 'es',
+        sourcemap: true,
       },
     ],
     plugins: [
+      resolve(),
       babel({
         exclude: ['node_modules/**,', '**/*.scss'],
       }),
-      resolve(),
       postcss({
-        extract: true,
-        plugins: [cssnano, url({url: 'inline'})],
+        extract: './dist/portfolio.css',
+        minimize: true,
+        sourceMap: true,
+        plugins: [url({url: 'inline'})],
       }),
     ],
   }
